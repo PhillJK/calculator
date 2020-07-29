4264a58b61e96b5601330311283835d4444e130c
PhillJK <farrux2315@gmail.com>
2020-07-29 17:21:47 +0500

    Some changes was made to the script and style

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # calculator
 This is a simple html/javascript calculator
+
+This calculator is from frontendMasters course (Complete intro to Web development v2)
+
+The master branch is written by me
+
+The solution branch is how Brian Holt(the teacher of the course) wrote the code
--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const nums = document.querySelectorAll(".number");
 const screen = document.querySelector(".screen");
 screen.innerText = "0";
 
+const operators = document.querySelectorAll(".operator");
+
+const equalButton = document.querySelector(".equal");
+
+const clearButton = document.querySelector(".clear")
+
+const backspaceButton = document.querySelector(".backspace")
+
+let number1;
+let number2;
+let opt;
+
+clearButton.addEventListener("click", function() {
+    number1 = "";
+    number2 = "";
+    opt = "";
+    screen.innerText = "0"
+});
+
+backspaceButton.addEventListener("click", function() {
+    if(screen.innerText != "") {
+        screen.innerText = screen.innerText.slice(0,-1);
+    } else {
+        screen.innerText = "0"; 
+    }
+});
+
+
+for(let operator of operators) {
+    operator.addEventListener("click", function(event) {
+        number1 = screen.innerText;
+        screen.innerText = "0";
+        opt = event.target.innerText;
+    });
+}
 
 for(let num of nums) {
     num.addEventListener("click", function(event) {
@@ -17,15 +52,20 @@ for(let num of nums) {
 function calculate(num1,num2,operator) {
     let result;
     if(operator == "+") {
-        result = num1 + num2;
+        result = parseInt(num1) + parseInt(num2);
     } else if(operator == "-") {
-        result = num1 - num2;
+        result = parseInt(num1) - parseInt(num2);
     } else if(operator == "*") {
-        result = num1 * num2;
+        result = parseInt(num1) * parseInt(num2);
     } else if(operator == "/") {
-        result = num1 / num2;
+        result = parseInt(num1) / parseInt(num2);
     } else {
         result = 0;
     }
     return result;
 }
+
+equalButton.addEventListener("click", function() {
+    number2 = screen.innerText;
+    screen.innerText = calculate(number1,number2,opt);
+});
